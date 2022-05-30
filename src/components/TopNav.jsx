4f405e250f46d7d4538fa31fb422ca7263fd42df
venilson1 <venilson2009@hotmail.com>
2022-05-30 16:31:50 -0300

    footer

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -45,7 +45,7 @@ export default function TopNav() {
 
           {dropDown &&
             <>
-              <div className="origin-top-right absolute right-96 top-10 mt-2 w-56 rounded-md 
+              <div className="origin-top-right absolute right-0 top-12 mt-2 w-56 rounded-md 
               shadow-lg dark:bg-gray-700 ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1">
                 <div className="py-1" role="none">
                   <button href="#" className="text-gray-100 block px-4 py-2 text-base hover:bg-gray-600 w-full hover:text-blue-300" role="menuitem" tabIndex="-1" id="menu-item-0">Stacks</button>
--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Footer from '../components/Footer'
 import TopNav from '../components/TopNav'
 
 export default function Layout() {
   return (
     <>
       <TopNav />
+      <Footer />
     </>
   )
 }
