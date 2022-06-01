da2e5c4215d63a88abec11502ebaac16cbbbe14d
venilson1 <venilson2009@hotmail.com>
2022-06-01 09:33:13 -0300

    update datas

--- src/components/Stacks.jsx
+++ src/components/Stacks.jsx
@@ -1,86 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { skills } from '../data/data'
 import spaceBetweenUnderline from '../utils/space'
 
 export default function Stacks() {
 
-  const image = "https://avatars.githubusercontent.com/u/57969262?v=4"
+  const [skillsFiltered, setSkillsFiltered] = useState(skills);
+
+  const handlerSkillsFilter = (type) => {
+
+    if (type === 'all') {
+      return setSkillsFiltered(skills)
+    } else {
+      const skillsByType = skills.filter(el => el.type === type);
+      type === 'all' ? setSkillsFiltered(skills) : setSkillsFiltered(skillsByType)
+    }
+  }
 
   return (
     <>
-      <div className="mt-10 h-screen">
-        <div className="text-center -space-y-3 mb-10">
-          <span className="text-5xl font-extrabold">Tecnologias</span>
-          <p className='underline'>{spaceBetweenUnderline}</p>
+      <div className="mt-10">
+        <div className="text-center">
+          <div className="-space-y-3">
+            <span className="text-5xl font-extrabold ">Tecnologias</span>
+            <p className='underline'>{spaceBetweenUnderline}</p>
+          </div>
+          <div className="mt-4 flex justify-center">
+            <div className="text-xl  w-4/12">
+              <span className="">Uma lista das minhas ferramentas e tecnologias favoritas que uso regularmente.</span>
+            </div>
+          </div>
         </div>
 
         <div className="my-10 flex justify-center ">
           <div className=" flex justify-center md:w-6/12 flex-wrap gap-2 md:gap-4">
-            <button className="bg-gray-800 focus:outline-none 
-              focus:ring focus:bg-green-100 focus:text-gray-900  focus:ring-blue-400 
+
+            <button onClick={() => handlerSkillsFilter('all')} className="bg-gray-800 focus:outline-none 
+              focus:ring focus:bg-blue-200 focus:text-gray-700  focus:ring-blue-400 
               rounded-full px-5 py-1 font-bold text-gray-400">
               Tudo
             </button>
 
-            <button className="bg-gray-800 focus:outline-none 
-              focus:ring focus:bg-stone-200 focus:text-gray-900  focus:ring-blue-400 
+            <button onClick={() => handlerSkillsFilter('development')} className="bg-gray-800 focus:outline-none 
+              focus:ring focus:bg-violet-300 focus:text-gray-700  focus:ring-violet-400 
               rounded-full px-5 py-1 font-bold text-gray-400">
               Desenvolvimento Web
             </button>
 
-            <button className="bg-gray-800 focus:outline-none 
-              focus:ring focus:bg-orange-100 focus:text-gray-900  focus:ring-blue-400 
+            <button onClick={() => handlerSkillsFilter('design')} className="bg-gray-800 focus:outline-none 
+              focus:ring focus:bg-orange-200 focus:text-gray-700  focus:ring-orange-300 
               rounded-full px-5 py-1 font-bold text-gray-400">
               Designer Web
             </button>
 
-            <button className="bg-gray-800 focus:outline-none 
-              focus:ring focus:bg-red-100 focus:text-gray-900  focus:ring-blue-400 
+            <button onClick={() => handlerSkillsFilter('devops')} className="bg-gray-800 focus:outline-none 
+              focus:ring focus:bg-red-200 focus:text-gray-700  focus:ring-red-300 
               rounded-full px-5 py-1 font-bold text-gray-400">
               Devops
             </button>
           </div>
         </div>
 
-        <div className="lg:mx-auto lg:w-6/12 px-2 flex flex-wrap gap-4 justify-center">
-          <div className="md:w-72 border border-gray-700 bg-gray-800 rounded-xl p-4 flex">
-            <div className="flex-none flex items-center px-2">
-              <img className="w-10 h-10 rounded-md" src={image} alt="Avatar of Jonathan Reinink" />
-            </div>
-            <div className="grow px-2">
-              <p className="text-gray-100 text-base font-bold">Java</p>
-              <p className="text-gray-100 text-sm mb-1">Desenvolvimento web</p>
-            </div>
-          </div>
+        <div className="lg:mx-auto lg:w-6/12 px-2 flex flex-wrap gap-6 justify-center my-16">
+          {
+            skillsFiltered.map(({ id, name, description, link, type, image }) => {
+              return (
 
-          <div className="md:w-72 border border-gray-700 bg-gray-800 rounded-xl p-4 flex">
-            <div className="flex-none flex items-center px-2">
-              <img className="w-10 h-10 rounded-md" src={image} alt="Avatar of Jonathan Reinink" />
-            </div>
-            <div className="grow px-2">
-              <p className="text-gray-100 text-base font-bold">Java</p>
-              <p className="text-gray-100 text-sm mb-1">Desenvolvimento web</p>
-            </div>
-          </div>
-
-          <div className="md:w-72 border border-gray-700 bg-gray-800 rounded-xl p-4 flex">
-            <div className="flex-none flex items-center px-2">
-              <img className="w-10 h-10 rounded-md" src={image} alt="Avatar of Jonathan Reinink" />
-            </div>
-            <div className="grow px-2">
-              <p className="text-gray-100 text-base font-bold">Java</p>
-              <p className="text-gray-100 text-sm mb-1">Desenvolvimento web</p>
-            </div>
-          </div>
-
-          <div className="md:w-72 border border-gray-700 bg-gray-800 rounded-xl p-4 flex">
-            <div className="flex-none flex items-center px-2">
-              <img className="w-10 h-10 rounded-md" src={image} alt="Avatar of Jonathan Reinink" />
-            </div>
-            <div className="grow px-2">
-              <p className="text-gray-100 text-base font-bold">Java</p>
-              <p className="text-gray-100 text-sm mb-1">Desenvolvimento web</p>
-            </div>
-          </div>
+                <div key={id} className="md:w-72 border border-gray-700 bg-gray-800 rounded-xl p-4 flex">
+                  <div className="flex-none flex items-center px-2 bg-slate-600 rounded-lg">
+                    <img className="w-8 h-8 rounded-md" src={image} alt={name} />
+                  </div>
+                  <div className="grow px-2">
+                    <p className="text-gray-100 text-base font-bold">{name}</p>
+                    <p className="text-gray-100 text-sm mb-1">{description}</p>
+                  </div>
+                </div>
+              )
+            })
+          }
         </div>
       </div>
 
--- src/data/data.js
+++ src/data/data.js
@@ -1,3 +1,24 @@
+import casting from "../assets/logos/casting.png";
+import proz from "../assets/logos/proz.png";
+import semfoto from "../assets/logos/sem-foto.png";
+import uninove from "../assets/logos/uninove.jpg";
+import devsfree from "../assets/logos/devsfree.png";
+
+import bootstrap from "../assets/skills/bootstrap.png";
+import css3 from "../assets/skills/css3.png";
+import heroku from "../assets/skills/heroku.png";
+import html5 from "../assets/skills/html-5.png";
+import java from "../assets/skills/java.png";
+import javascript from "../assets/skills/javascript.png";
+import mongodb from "../assets/skills/mongodb.png";
+import nodejs from "../assets/skills/nodejs.png";
+import postgres from "../assets/skills/postgresql.png";
+import react from "../assets/skills/react.png";
+import springboot from "../assets/skills/springboot.png";
+import tailwind from "../assets/skills/tailwind.png";
+import typescript from "../assets/skills/typescript.png";
+import vscode from "../assets/skills/vscode.png";
+
 export const carrer = [
   {
     id: 1,
@@ -6,25 +27,37 @@ export const carrer = [
     techs: [".NET", "SQL Server", "VueJs", "Java Script"],
     initial: "2020",
     final: "2020",
-    image: "assets/logos/casting.png",
+    image: casting,
   },
   {
     id: 2,
-    company: "Proz",
-    occupation: "professor Java",
-    techs: ["Java", "Phyton"],
-    initial: "2020",
-    final: "2021",
-    image: "assets/logos/proz.png",
-  },
-  {
-    id: 3,
     company: "Free Lancer",
     occupation: "programador",
     techs: ["Java", "React", "Postgres", "NodeJs", "Heroku"],
     initial: "2022",
     final: "momento",
-    image: "assets/logos/sem-foto.png",
+    image: semfoto,
+  },
+];
+
+export const voluntary = [
+  {
+    id: 1,
+    company: "Proz",
+    occupation: "professor Java",
+    techs: ["Java", "Phyton"],
+    initial: "2020",
+    final: "2021",
+    image: proz,
+  },
+  {
+    id: 2,
+    company: "Devs Free",
+    occupation: "Desenvolvedor Java",
+    techs: ["Java", "Docker", "AWS"],
+    initial: "2020",
+    final: "momento",
+    image: devsfree,
   },
 ];
 
@@ -35,6 +68,7 @@ export const institutes = [
     course: "Análise e Desenvolvimento de Sistemas",
     initiation: "2019",
     conclusion: "2021",
+    image: uninove,
   },
 ];
 
@@ -42,113 +76,113 @@ export const skills = [
   {
     id: 1,
     name: "React",
-    description: "Web development",
+    description: "Desenvolvimento web",
     link: "https://reactjs.org/",
     type: "development",
-    image: "assets/skills/react.png",
+    image: react,
   },
   {
     id: 2,
     name: "Javascript",
-    description: "Web development",
+    description: "Desenvolvimento web",
     link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
     type: "development",
-    image: "assets/skills/javascript.png",
+    image: javascript,
   },
   {
     id: 3,
     name: "Typescript",
-    description: "Web development",
+    description: "Desenvolvimento web",
     link: "https://www.typescriptlang.org/",
     type: "development",
-    image: "assets/images/skills/typescript.png",
+    image: typescript,
   },
   {
-    id: 3,
+    id: 4,
     name: "Html5",
     description: "Web layouts",
     link: "https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5",
     type: "development",
-    image: "assets/images/skills/html-5.png",
+    image: html5,
   },
   {
-    id: 4,
+    id: 5,
     name: "Css3",
     description: "Web design",
     link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
     type: "design",
-    image: "assets/images/skills/css3.png",
+    image: css3,
   },
   {
-    id: 5,
+    id: 6,
     name: "Bootstrap",
     description: "Web design",
     link: "https://getbootstrap.com/",
     type: "design",
-    image: "assets/images/skills/bootstrap.png",
+    image: bootstrap,
   },
   {
-    id: 6,
+    id: 7,
     name: "Postgres",
     description: "Database",
     link: "https://www.postgresql.org/",
     type: "database",
-    image: "assets/images/skills/postgresql.png",
+    image: postgres,
   },
   {
-    id: 7,
+    id: 8,
     name: "Heroku",
     description: "Devops",
     link: "https://www.heroku.com/",
     type: "devops",
-    image: "assets/images/skills/heroku.png",
-  },
-  {
-    id: 8,
-    name: "Visual Studio Code",
-    description: "Code editor of choice",
-    link: "https://code.visualstudio.com/",
-    type: "development",
-    image: "assets/images/skills/vscode.png",
+    image: heroku,
   },
   {
     id: 9,
-    name: "Java",
-    description: "Web development",
-    link: "https://www.oracle.com/br/java/",
+    name: "Visual Studio Code",
+    description: "Editor de código",
+    link: "https://code.visualstudio.com/",
     type: "development",
-    image: "assets/images/skills/java.png",
+    image: vscode,
   },
   {
     id: 10,
-    name: "Spring Boot",
-    description: "Web development",
-    link: "https://spring.io/projects/spring-boot",
+    name: "Java",
+    description: "Desenvolvimento web",
+    link: "https://www.oracle.com/br/java/",
     type: "development",
-    image: "assets/images/skills/springboot.png",
+    image: java,
   },
   {
     id: 11,
+    name: "Spring Boot",
+    description: "Desenvolvimento web",
+    link: "https://spring.io/projects/spring-boot",
+    type: "development",
+    image: springboot,
+  },
+  {
+    id: 12,
     name: "Mongodb",
     description: "Database",
     link: "https://www.mongodb.com",
     type: "database",
-    image: "assets/images/skills/mongodb.png",
-  },
-  {
-    id: 12,
-    name: "NodeJs",
-    description: "Web development",
-    link: "https://nodejs.org/en/",
-    type: "development",
-    image: "assets/images/skills/nodejs.png",
+    image: mongodb,
   },
   {
     id: 13,
+    name: "NodeJs",
+    description: "Desenvolvimento web",
+    link: "https://nodejs.org/en/",
+    type: "development",
+    image: nodejs,
+  },
+  {
+    id: 14,
     name: "Tailwind",
     description: "Web design",
     link: "https://tailwindcss.com/",
     type: "design",
-    image: "assets/images/skills/tailwind.png",
+    image: tailwind,
   },
 ];
