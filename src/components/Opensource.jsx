a67a86ddb4ff5e3f885583c01beecf08f6c88d0e
venilson1 <venilson2009@hotmail.com>
2022-06-03 18:32:04 -0300

    loader

--- src/components/Opensource.jsx
+++ src/components/Opensource.jsx
@@ -2,18 +2,21 @@ import React, { useEffect, useState } from 'react'
 import spaceBetweenUnderline from '../utils/space'
 import { AiOutlineGithub } from 'react-icons/ai'
 import api from '../data/api';
+import ClipLoader from "react-spinners/ClipLoader";
 
 export default function Opensource() {
 
   const [repos, setRepos] = useState();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true)
     api
       .get("/repos")
       .then((response) => setRepos(response.data))
       .catch((err) => {
         console.error("ops! ocorreu um erro " + err);
-      });
+      }).finally(setIsLoading(false));
   }, []);
 
   return (
@@ -29,25 +32,28 @@ export default function Opensource() {
       <div className="lg:mx-auto lg:w-8/12 mt-10 flex justify-center flex-wrap gap-6 my-10">
 
         {
-          repos?.map(({ id, name, html_url, description, topics }) => {
-            return (
-              <div key={id} className="lg:w-96 border border-gray-700 bg-gray-800 rounded-xl p-4 
+          isLoading ?
+            <div className="h-72">
+              <ClipLoader size={150} color={'rgb(209 213 219)'} />
+            </div> : (repos?.map(({ id, name, html_url, description, topics }) => {
+              return (
+                <div key={id} className="w-96 border border-gray-700 bg-gray-800 rounded-xl p-4 
               flex flex-col justify-between">
-                <div className="flex items-center space-x-2">
-                  <AiOutlineGithub />
-                  <a href={html_url} target="_blank" rel="noreferrer" className="font-bold text-base cursor-pointer text-blue-400">{name}</a>
+                  <div className="flex items-center space-x-2">
+                    <AiOutlineGithub />
+                    <a href={html_url} target="_blank" rel="noreferrer" className="font-bold text-base cursor-pointer text-blue-400">{name}</a>
+                  </div>
+                  <div className="py-1 flex flex-wrap">
+                    {
+                      topics.map((el, i) => {
+                        return <span key={i} className="bg-gray-100 text-gray-800 text-xs mr-2 px-2 rounded dark:bg-gray-700 dark:text-gray-300">{el}</span>
+                      })
+                    }
+                  </div>
+                  <p className="text-gray-400 text-sm text-ellipsis overflow-hidden">{description}</p>
                 </div>
-                <div className="py-1 flex flex-wrap">
-                  {
-                    topics.map((el, i) => {
-                      return <span key={i} className="bg-gray-100 text-gray-800 text-xs mr-2 px-2 rounded dark:bg-gray-700 dark:text-gray-300">{el}</span>
-                    })
-                  }
-                </div>
-                <p className="text-gray-400 text-sm text-ellipsis overflow-hidden">{description}</p>
-              </div>
-            )
-          })
+              )
+            }))
         }
       </div>
 
